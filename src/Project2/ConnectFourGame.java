6fd66055e4880095e65ac3c288f67f5488105f22
dstro <dstro@35.40.212.128>
2018-02-01 00:24:37 -0500

    Reworked the draw method of the game. Created the first version of AI has one major flaw.

--- src/Project2/ConnectFourGame.java
+++ src/Project2/ConnectFourGame.java
@@ -20,9 +20,13 @@ public class ConnectFourGame {
 		player = 0;
 	}
 	
+	public int[][] getBoard(){
+		return board;
+	}
+	
 	public int selectCol(int pCol) {
 		
-		for (int i = 0; i < size; i++) {
+		for (int i = size - 1; i >= 0; i--) {
 			if (board[pCol][i] == -1) {
 				board[pCol][i] = player;
 				return i;
@@ -30,6 +34,38 @@ public class ConnectFourGame {
 		}
 		
 		return -1;
+	}
+	
+	public boolean canPlay(int x, int y) {
+		
+		boolean canplay = false;
+		
+		for (int i = size - 1; i >= 0; i--) {
+			if (board[x][i] == -1) {
+				if (y == i) {
+					return true;
+				}else {
+					break;
+				}
+			}
+		}
+		
+		return canplay;
+		
+	}
+	
+	public String toString() {
+		
+		String s = "";
+		
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {	
+				s += Integer.toString(board[x][y]);
+			}
+			s += "\n";
+		}
+		
+		return s;
 	}
 	
 	public void reset() {
@@ -51,6 +87,252 @@ public class ConnectFourGame {
 		return player;
 	}
 	
+	public int getPlayer() {
+		return player;
+	}
+		
+	
+	public int runAI() {
+		
+		int[][] boardScore = getBoardScore();
+		int highX = 0;
+		int highY = 0;
+		int highscore = 0;
+		
+		for (int x = 0; x < size ; x++) {
+			for (int y = 0; y < size; y++) {
+				
+				if (canPlay(x, y)) {
+					
+					if (Math.abs(boardScore[x][y]) > Math.abs(highscore)) {
+						
+						highscore = boardScore[x][y];
+						highX = x;
+						highY = y;
+					
+					}
+					
+				}
+				
+			}
+				
+		}
+		
+		System.out.println(highscore);
+		System.out.println(highX);
+		System.out.println(highY);
+		
+		selectCol(highX);
+		return highX;
+		
+	}
+	
+	public int[][] getBoardScore() {
+		
+		int[][] boardScore = new int[size][size];
+		
+		for (int x = 0; x < size ; x++) {
+			for (int y = 0; y < size; y++) {
+				
+				if (board[x][y] == -1) {
+					
+					//if there is no player then the score is 1
+					boardScore[x][y] += 1;
+					
+				}else {
+					
+					//Player chip the the chip we are looking at.
+					int playerChip = -1;
+					
+					
+					//This assigns a player value to the chip for future reference.
+					if (board[x][y] == USER) {			
+						playerChip = USER;
+					}else {
+						playerChip = COMPUTER;
+					}
+					
+					//this is how many chips away we are searching
+					int distance = 1;
+					
+					//the next empty chip will be 5 points (+ or - based on player)
+					int nextScore = 5;
+					int inRowValue = 5;
+					
+					//Convert score to negative if it was a players chip
+					if (playerChip == USER) {
+						nextScore *= -1;
+						inRowValue *= -1;
+					}
+
+					
+					//search positive x values (Make sure we are not on the edge)
+					if (x + 1 < size) {
+						while(board[x + distance][y] == playerChip) {
+							
+							nextScore += inRowValue;
+							
+							if (x + (distance + 1) >= size) {
+								break;
+							}else {
+								distance += 1;
+							}
+							
+						}
+					}
+					
+					//Add nextScore to the open slot if its not out of bounds
+					if (x + (distance) < size) {
+						boardScore[x + distance][y] += nextScore;
+					}
+					
+					
+					//reset for next check
+					nextScore = 0 + inRowValue;
+					distance = 1;
+					
+					//search negative x values (Make sure we are not on the edge)
+					if (x - 1 > -1) {
+						while(board[x - distance][y] == playerChip) {
+							
+							nextScore += inRowValue;
+							
+							if (x - (distance + 1) <= -1) {
+								break;
+							}else {
+								distance += 1;
+							}
+							
+						}
+					}
+					
+					//Add nextScore to the open slot if its not out of bounds
+					if (x - (distance) > -1) {
+						boardScore[x - distance][y] += nextScore;
+					}
+					
+					//reset for next check
+					nextScore = 0 + inRowValue;
+					distance = 1;
+					
+					//search positive y values (Make sure we are not on the edge)
+					if (y + 1 < size) {
+						while(board[x][y + distance] == playerChip) {
+							
+							nextScore += inRowValue;
+							
+							if (y + (distance + 1) >= size) {
+								break;
+							}else {
+								distance += 1;
+							}
+							
+						}
+					}
+					
+					//Add nextScore to the open slot if its not out of bounds
+					if (y + (distance) < size) {
+						boardScore[x][y + distance] += nextScore;
+					}
+								
+					//reset for next check
+					nextScore = 0 + inRowValue;
+					distance = 1;
+					
+					//search negative y values (Make sure we are not on the edge)
+					if (y - 1 > -1) {
+						while(board[x][y - distance] == playerChip) {
+							
+							nextScore += inRowValue;
+							
+							if (y - (distance + 1) <= -1) {
+								break;
+							}else {
+								distance += 1;
+							}
+							
+						}
+					}
+					
+					//Add nextScore to the open slot if its not out of bounds
+					if (y - (distance) > -1) {
+						boardScore[x][y - distance] += nextScore;
+					}
+					
+					//reset for next check
+					nextScore = 0 + inRowValue;
+					distance = 1;
+					
+					//search negative diagonal values (Make sure we are not on the edge)
+					if (y - 1 > -1 && x - 1 > -1) {
+						while(board[x - distance][y - distance] == playerChip) {
+							
+							nextScore += inRowValue;
+							
+							if (y - (distance + 1) <= -1 || x - (distance + 1) <= -1) {
+								break;
+							}else {
+								distance += 1;
+							}
+							
+						}
+					}
+					
+					//Add nextScore to the open slot if its not out of bounds
+					if (y - (distance) > -1 && x - (distance) > -1) {
+						boardScore[x - distance][y - distance] += nextScore;
+					}
+					
+					//reset for next check
+					nextScore = 0 + inRowValue;
+					distance = 1;
+					
+					//search positive diagonal values (Make sure we are not on the edge)
+					if (y - 1 > -1 && x + 1 < size) {
+						while(board[x + distance][y - distance] == playerChip) {
+							
+							nextScore += inRowValue;
+							
+							if (y - (distance + 1) <= -1 || x + (distance + 1) >= size) {
+								break;
+							}else {
+								distance += 1;
+							}
+							
+						}
+					}
+					
+					//Add nextScore to the open slot if its not out of bounds
+					if (y - (distance) > -1 && x + (distance) < size) {
+						boardScore[x + distance][y - distance] += nextScore;
+					}
+					
+				}
+				
+
+				
+			}
+			
+		}
+		
+		for (int x = 0; x < size; x++) {
+			for(int y=0; y< size; y++) {
+				
+				if (board[x][y] == 0) {
+					boardScore[x][y] = 0;
+				}
+				
+				if (board[x][y] == 1) {
+					boardScore[x][y] = 0;
+				}
+				
+			}
+		}
+		
+		return boardScore;
+		
+	}
+	
 	public boolean isWinner( int person ) {
 		
 		for (int x = 0; x < size; x++) {
@@ -62,10 +344,11 @@ public class ConnectFourGame {
 					if (y + 3 < size) {
 						while(board[x][y + inRowVertical] == person) {
 							inRowVertical += 1;
-						}
-						
-						if (inRowVertical >= 4) {
-							return true;
+							
+							if (inRowVertical >= 4) {
+								return true;
+							}
+							
 						}
 					}
 					
@@ -74,11 +357,13 @@ public class ConnectFourGame {
 					if (x + 3 < size) {
 						while(board[x + inRowHorizontal][y] == person) {
 							inRowHorizontal += 1;
+							
+							if (inRowHorizontal >= 4) {
+								return true;
+							}
+							
 						}
 						
-						if (inRowHorizontal >= 4) {
-							return true;
-						}
 					}
 			
 					int inRowDiag = 1;
@@ -86,23 +371,27 @@ public class ConnectFourGame {
 					if (x + 3 < size && y + 3 < size) {
 						while(board[x + inRowDiag][y + inRowDiag] == person) {
 							inRowDiag += 1;
+						
+							if (inRowDiag >= 4) {
+								return true;
+							}
+							
 						}
 						
-						if (inRowDiag >= 4) {
-							return true;
-						}
 					}
 					
                     inRowDiag = 1;
 					
-					if (x - 3 < size && y + 3 < size) {
+					if (x - 3 > 0 && y + 3 < size) {
 						while(board[x - inRowDiag][y + inRowDiag] == person) {
 							inRowDiag += 1;
+						
+							if (inRowDiag >= 4) {
+								return true;
+							}
+							
 						}
 						
-						if (inRowDiag >= 4) {
-							return true;
-						}
 					}
 					
 				}
--- src/Project2/ConnectFourPannel.java
+++ src/Project2/ConnectFourPannel.java
@@ -11,96 +11,131 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-public class ConnectFourPannel extends JFrame implements ActionListener{
+public class ConnectFourPannel extends JFrame implements ActionListener {
 
-	private	JLabel[][]	board;		/**	GUI	visual	for	the	game	board	model	*/
-	private	JButton[]	selection;	/**	buttons	to	select	game	columns	*/
-	private	ConnectFourGame	game;	/**	reference	to	the	game	model	*/
+	private JLabel[][] board;
+	/** GUI visual for the game board model */
+	private JButton[] selection;
+	/** buttons to select game columns */
+	private ConnectFourGame game;
+	/** reference to the game model */
 	private int boardSize;
-	ImageIcon openImage; 
-	ImageIcon playerImage; 
-	ImageIcon computerImage;
 	
+	ImageIcon openImage;
+	ImageIcon playerImage;
+	ImageIcon computerImage;
+
 	public ConnectFourPannel() {
-		
+
 		this.setTitle("Connect Four Game");
-		
-		int bSize =	10;
-		String	strBdSize;
-		strBdSize =	JOptionPane.showInputDialog(null,	"Enter	board	size",	bSize);	
-		bSize =	Integer.parseInt(strBdSize);
+
+		int bSize = 10;
+		String strBdSize;
+		strBdSize = JOptionPane.showInputDialog(null, "Enter	board	size", bSize);
+		bSize = Integer.parseInt(strBdSize);
 		boardSize = bSize;
 		game = new ConnectFourGame(bSize);
-		
-		
-		this.setSize(800, 700);
-		
-		openImage = new ImageIcon(new ImageIcon("OpenSpace.png").getImage().getScaledInstance(this.getWidth() / 10, this.getHeight() / 11, Image.SCALE_DEFAULT));
-		playerImage = new ImageIcon(new ImageIcon("PlayerSpace.png").getImage().getScaledInstance(this.getWidth() / 10, this.getHeight() / 11, Image.SCALE_DEFAULT));
-		computerImage = new ImageIcon(new ImageIcon("ComputerSpace.png").getImage().getScaledInstance(this.getWidth() / 10, this.getHeight() / 11, Image.SCALE_DEFAULT));
 
+		this.setSize(800, 700);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		openImage = new ImageIcon(new ImageIcon("OpenSpace.png").getImage().getScaledInstance(this.getWidth() / 10,
+				this.getHeight() / 11, Image.SCALE_DEFAULT));
+		
+		playerImage = new ImageIcon(new ImageIcon("PlayerSpace.png").getImage().getScaledInstance(this.getWidth() / 10,
+				this.getHeight() / 11, Image.SCALE_DEFAULT));
+		
+		computerImage = new ImageIcon(new ImageIcon("ComputerSpace.png").getImage()
+				.getScaledInstance(this.getWidth() / 10, this.getHeight() / 11, Image.SCALE_DEFAULT));
+
 		GridLayout layout = new GridLayout(bSize + 1, bSize);
 		this.setLayout(layout);
-		
+
 		selection = new JButton[bSize];
 		for (int x = 0; x < bSize; x++) {
 			selection[x] = new JButton("Select");
 			selection[x].addActionListener(this);
 			this.add(selection[x]);
 		}
-		
 
 		board = new JLabel[bSize][bSize];
-		for (int x = 0; x < bSize; x++) {
-			for (int y = 0; y < bSize; y++) {
+		for (int y = 0; y < bSize; y++) {
+			for (int x = 0; x < bSize; x++) {
 				board[x][y] = new JLabel("");
 				board[x][y].setIcon(openImage);
 				board[x][y].setHorizontalAlignment(JLabel.CENTER);
+				board[x][y].setVerticalAlignment(JLabel.CENTER);
 				this.add(board[x][y]);
 			}
 		}
-		
-		
+
 		this.setVisible(true);
 	}
 
-	
 	public void actionPerformed(ActionEvent e) {
-		
+
 		Object obj = e.getSource();
-		
-		for(int num = 0; num < selection.length; num++) {
+
+		for (int num = 0; num < selection.length; num++) {
 			if (obj == selection[num]) {
-				
-				//We draw from top down but need 
-				//to display from bottom up
-				int pos = (boardSize - game.selectCol(num)) - 1;
-				
-				if (pos < 10) {
-					
-					if(game.switchPlayer() == 0) {
-						board[pos][num].setIcon(computerImage);
-						if (game.isWinner(1)) {
-							JOptionPane.showMessageDialog(this, "The Computer has won!");
-							//game.reset();
-						}
-					}else {
-						board[pos][num].setIcon(playerImage);
-						if (game.isWinner(0)) {
-							JOptionPane.showMessageDialog(this, "You have won!");
-							//game.reset();
-						}
+
+				// We draw from top down but need
+				// to display from bottom up
+				if (game.getPlayer() == 0) {
+
+					game.selectCol(num);
+					redraw();
+					if (game.isWinner(0)) {
+						JOptionPane.showMessageDialog(this, "You have won!");
 					}
+					game.switchPlayer();
 					
-				}else {
-					JOptionPane.showMessageDialog(this, "Invalid Move");
+					game.runAI();
+					redraw();
+					if (game.isWinner(1)) {
+						JOptionPane.showMessageDialog(this, "You have lost!");
+					}
+					game.switchPlayer();
+					displayAIVision();
+					
+					System.out.println(game.toString());
+					
 				}
-				
+
+			}
+		}
+
+	}
+	
+	public void redraw() {
+		
+		for (int y = 0; y < boardSize; y ++) {			
+			for (int x = 0; x < boardSize; x ++) {
+
+				if (game.getBoard()[x][y] == -1) {
+					board[x][y].setIcon(openImage);
+				}else if (game.getBoard()[x][y] == 0) {
+					board[x][y].setIcon(playerImage);
+				}else if (game.getBoard()[x][y] == 1) {
+					board[x][y].setIcon(computerImage);
+				}
 			}
 		}
 		
 	}
 	
-	
+	public void displayAIVision() {
+		
+		int[][] boardScore = game.getBoardScore();
+		
+		for (int y = 0; y < boardSize; y ++) {
+			for (int x = 0; x < boardSize; x ++) {
+				
+				board[x][y].setText(Integer.toString(boardScore[x][y]));
+						
+			}
+		}
+		
+	}
+
 }
