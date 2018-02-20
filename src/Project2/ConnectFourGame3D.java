eccfc55165126b4fbf59a6087640e24610c717be
dyl10s <dylanstrohschein@gmail.com>
2018-02-19 21:44:31 -0500

    Starting to make changes for 3d version

--- src/Project2/ConnectFourGame3D.java
+++ src/Project2/ConnectFourGame3D.java
@@ -2,75 +2,57 @@ package Project2;
 
 public class ConnectFourGame3D {
 
-	private int[][][]	board;
+	private ConnectFourGame[] board;
 	private int size;
 	private int player;	
 	public static final int USER =	0;
 	public static final int COMPUTER =	1; 
 	
 	public ConnectFourGame3D(int size){
-		
+	
 		this.size = size;
 		
-		board = new int[size][size][size];
-		
-		for (int x = 0; x < size; x++){
-			for (int y = 0; y < size; y++){
-				for (int z = 0; z < size; z++){
-					
-					board[x][y][z] = -1;
-					
-				}	
-			}	
+		board = new ConnectFourGame[size];
+		for (ConnectFourGame b: board){
+			b = new ConnectFourGame(size);
 		}
+		
+		System.out.println(this.toString());
 		
 	}
 	
-	public int[][][] getBoard(){
+	public ConnectFourGame[] getBoard(){
 		return board;
 	}
 	
 	public int selectCol(int pX, int pY) {
 		
-		for (int i = size - 1; i >= 0; i--) {
-			if (board[pX][pY][i] == -1) {
-				board[pX][pY][i] = player;
-				return i;
-			}
-		}
+		return board[pX].selectCol(pY);
 		
-		return -1;
 	}
 	
 	public boolean canPlay(int x, int y, int z) {
 		
-		boolean canplay = false;
-		
-		for (int i = size - 1; i >= 0; i--) {
-			if (board[x][y][i] == -1) {
-				if (z == i) {
-					return true;
-				}else {
-					break;
-				}
-			}
-		}
-		
-		return canplay;
+		return board[x].canPlay(y, z);
 		
 	}
 	
+	public String toString(){
+		
+		String returnS = "";
+		
+		for (ConnectFourGame b: board){
+			returnS += b.toString();
+		}
+		
+		return returnS;
+		
+	}
 	
 	public void reset() {
 		
-		for (int x = 0; x < size; x++){
-			for (int y = 0; y < size; y++){
-				for (int z = 0; z < size; z++){
-					
-					board[x][y][z] = -1;
-					
-				}	
-			}	
+		for (ConnectFourGame b: board){
+			b.reset();
 		}
 		
 	}
